be184282eb4ea93c55809dda6ca58401a1a50a56
Max Paez <maxpaez.coderhouse@gmail.com>
2023-02-23 21:37:59 -0300

    actualizando gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
+import Footer from "./Test/Footer";
 import ListadoDeProductosContainer from "./Test/ListadoDeProductosContainer";
 // import "../src/style-sheets/App.css";
 
