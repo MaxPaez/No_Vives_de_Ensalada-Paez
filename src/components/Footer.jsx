ebdfef427523ab174331bc14cd93ad402130f8da
Max Paez <maxpaez.coderhouse@gmail.com>
2023-02-23 21:38:18 -0300

    actualizando gitignore

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "@mui/material";
-
-export default function Footer() {
-  return (
-    <Stack direction="row" justifyContent={"center"}>
-      <p>Este es un Footer fachero</p>
-    </Stack>
-  );
-}
