f7b72b952e0a085f32a68796e704d03e1b5237bd
Max <maxpaez.coderhouse@gmail.com>
2023-03-03 15:01:31 -0300

    implementando componentes para los items

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import ItemListContainer from "./components/ItemListContainer";
+// import ItemListContainer from "./components/ItemListContainer2";
 import NavBar from "./components/NavBar";
+import ItemListContainer from "./test/ItemListContainer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting={"No Vives de Ensaladas"} />
+      {/* <ItemListContainer greeting={"No Vives de Ensaladas"} /> */}
+      <ItemListContainer />
     </div>
   );
 }
