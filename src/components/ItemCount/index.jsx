b1ab3d5511342d6de90899fb8c1799998a182e22
Max Paez <maxpaez.coderhouse@gmail.com>
2023-03-16 16:16:53 -0300

    .

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 
--- src/components/ItemDetailContainer/index.jsx
+++ src/components/ItemDetailContainer/index.jsx
@@ -18,7 +18,7 @@ function ItemDetailContainer({ itemRoute, itemId }) {
           );
           setDetalles(productoEncontrado);
         } else {
-          setDetalles("El producto no fue encontrado");
+          setDetalles(console.log("El producto no fue encontrado"));
         }
       })
       .catch((err) => console.log(err));
