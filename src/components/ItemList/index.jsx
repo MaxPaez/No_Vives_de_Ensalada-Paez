bacee756be889ad5664ea8a0c5eb4564c766e97a
Max <maxpaez.coderhouse@gmail.com>
2023-04-05 12:00:37 -0300

    ajustes menores

--- src/components/ItemList/index.jsx
+++ src/components/ItemList/index.jsx
@@ -1,13 +1,39 @@
 import Item from "../Item";
 
-function ItenList({ productos }) {
+function ProductGroup({ categoria, products }) {
   return (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
-      {productos.map((producto) => (
-        <Item key={producto.id} producto={producto} />
+    <div>
+      <h2>{categoria}</h2>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {products.map((producto) => (
+          <Item key={producto.id} producto={producto} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ItemList({ productos }) {
+  const productGroups = productos.reduce((groups, producto) => {
+    const categoria = producto.categoria;
+    if (!groups[categoria]) {
+      groups[categoria] = [];
+    }
+    groups[categoria].push(producto);
+    return groups;
+  }, {});
+
+  return (
+    <div>
+      {Object.keys(productGroups).map((categoria) => (
+        <ProductGroup
+          key={categoria}
+          categoria={categoria}
+          products={productGroups[categoria]}
+        />
       ))}
     </div>
   );
 }
 
-export default ItenList;
+export default ItemList;
